607b3f8bc265018b52a0f62848adc55ec76013d5
abhiinavgupta <121946359+abhiinavgupta@users.noreply.github.com>
2023-05-28 23:28:04 +0530

    for vercel

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-        {/* <title>AUTHENTIFICATION - ABHINAV GUPTA</title> */}
+        {/* <title>AUTHENTIFICATION - ABHINAV GUPTAa</title> */}
       
       <body>
         <Main />
